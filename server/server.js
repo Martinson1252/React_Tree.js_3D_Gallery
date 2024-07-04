b0b1b4204402fee47309eb45fc10ffb1c9a51be9
Martinson1252 <martinson1202@gmail.com>
2024-07-04 16:48:45 +0200

    Update server.js

--- server/server.js
+++ server/server.js
@@ -9,25 +9,34 @@ const serverless = require('serverless-http');
 
 const PORT = process.env.PORT || 8080;
 // app.listen(PORT, ()=> console.log("server started") );
-app.use('https://react-tree-3d-gallery-server.netlify.app/', router );
+app.use('https://react-tree-3d-gallery-server.netlify.app/' );
 module.exports.handler = serverless(app);
 const model_list = [];
 fs = require("fs");
 let folder = path.join("public/models/");
 const files = fs.readdirSync(folder);
-for (const i of files){
-    if(i.endsWith(".glb"))
-    {
-        
-        filesize = fs.statSync(path.join(  folder,i )).size/(1024*1024);
+
+const get = new Promise(    ()=>{
+    
+    for (const i of files){
+        if(i.endsWith(".glb"))
+        {
+            
+            filesize = fs.statSync(path.join(  folder,i )).size/(1024*1024);
         model_list.push( {fileName: i, fileSize: filesize, pathToFolder: folder});
     }
+
+}});
+
+
+
+const sent = new Promise( ()=> {
     
-}
-
-router.get("/api/items", (req, res) => {
-    res.send((model_list)); 
-
-   
+    app.get("/api/items", (req, res) => {
+        res.send((model_list)); 
+        console.log(model_list);
+    
+    });
 });
-    
+
+Promise.all(get,sent);
